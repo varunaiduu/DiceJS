05cfd70372669c1833acc0fcc1978818316124a7
varunaiduu <137759117+varunaiduu@users.noreply.github.com>
2023-07-11 12:28:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1 =Math.floor((Math.random()*6)+1);
 var randomImage= "dice" + randomNumber1+".png";
-var randomSource="images/"+randomImage;
 
-document.querySelectorAll("img")[0].setAttribute("src",randomSource);
+
+document.querySelectorAll("img")[0].setAttribute("src",randomImage);
 
 var randomNumber2 =Math.floor((Math.random()*6)+1);
-var Randomsource2="images/dice"+randomNumber2+".png"
+var Randomsource2="dice"+randomNumber2+".png"
 document.querySelectorAll("img")[1].setAttribute("src",Randomsource2);
 
 if(randomNumber1 > randomNumber2){
